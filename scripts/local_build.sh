3843b1e941ff573581923abe7be3f45043716677
Achilles Benetopoulos <abenetopoulos@gmail.com>
2024-11-30 13:52:41 -0800

    Fix home path in build script

--- scripts/local_build.sh
+++ scripts/local_build.sh
@@ -1,7 +1,15 @@
-cd ~/DRust_home/DRust/comm-lib
+#!/usr/bin/env bash
+set -e
+
+if [[ -z "${DRUST_HOME}" ]]; then
+  DRUST_HOME=$HOME/DRust/
+fi
+
+cd $DRUST_HOME/comm-lib
 make clean
 make -j lib
-cp libmyrdma.a ../drust/
-cd ~/DRust_home/DRust/drust
-~/.cargo/bin/cargo clean --release -p drust
-~/.cargo/bin/cargo build --release
+cp libmyrdma.a $DRUST_HOME/drust/
+
+cd $DRUST_HOME/drust
+cargo clean --release -p drust
+cargo build --release
